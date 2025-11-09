aeb1668b2423b3b897aed7661ee79beb8044d5cd
surya charan <psaisuryacharan@gmail.com>
2025-11-09 09:42:46 +0530

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App = () => (
             <Route
               path="/"
               element={
-                isExtension ? <CompactDashboard /> : <PairAddressInput />
+                isExtension ? <CompactDashboard /> : <Index />
               }
             />
 
